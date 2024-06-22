d9db6aa807c54e843b3935fe0a7b7591a5b4e5d9
Raman2114 <122012512+Raman2114@users.noreply.github.com>
2024-06-22 07:13:14 +0000

    datatypes summary

--- 01_basics/operations.js
+++ 01_basics/operations.js
@@ -8,3 +8,27 @@ console.log(2+3);
 console.log(2-3);
 console.log(2/3);//modulus
 
+//memory : stack(primitive)   any change in value leads to change in the copy of value
+// heap (non-primitive)   leads to change the  original value reference
+
+
+let a="raman";
+let b=a;
+console.log(a);
+b="rahul";
+console.log(b);
+
+
+
+let objectOne={
+    name:"raman",
+    email:"rpk2114@gmail.com",
+}
+
+let objectTwo =objectOne;
+console.log(objectTwo);
+
+objectTwo.name="komal";
+console.log(objectTwo);
+console.log(objectOne);
+
